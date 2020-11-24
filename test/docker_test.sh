b37349518759ada0c99d305a88c9f854f01071f7
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-11-24 03:44:08 +0100

    ✅ Add docker-compose tests
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

---
+++ test/docker_test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+echo "Waiting to ensure everything is fully ready for the tests..."
+sleep 60
+
+if ! ping -c 10 -q coturn ; then
+    echo 'Coturn Main container is not responding!'
+    # TODO Display logs to help bug fixing
+    #echo 'Check the following logs for details:'
+    #tail -n 100 logs/*.log
+    exit 2
+fi
+
+# XXX Add your own tests
+# https://docs.docker.com/docker-hub/builds/automated-testing/
+#echo "Executing Coturn app tests..."
+## TODO Test result of tests
+
+# Success
+echo 'Docker tests successful'
+exit 0
