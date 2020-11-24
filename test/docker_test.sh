6ec58a20204013892ec66ca8ddd06a687851d945
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-11-24 13:51:02 +0100

    💚 Disable ping since Coturn runs on host network
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- test/docker_test.sh
+++ test/docker_test.sh
@@ -3,15 +3,16 @@
 set -e
 
 echo "Waiting to ensure everything is fully ready for the tests..."
-sleep 120
+sleep 60
 
-if ! ping -c 10 -q coturn ; then
-    echo 'Coturn Main container is not responding!'
-    # TODO Display logs to help bug fixing
-    #echo 'Check the following logs for details:'
-    #tail -n 100 logs/*.log
-    exit 2
-fi
+echo 'TODO Test that Coturn (using network host) is responding!'
+#if ! ping -c 10 -q coturn ; then
+#    echo 'Coturn Main container is not responding!'
+#    # TODO Display logs to help bug fixing
+#    #echo 'Check the following logs for details:'
+#    #tail -n 100 logs/*.log
+#    exit 2
+#fi
 
 # XXX Add your own tests
 # https://docs.docker.com/docker-hub/builds/automated-testing/
